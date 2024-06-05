12d31d8c4f3d57ede1690f7ce35ab7e9744617ed
0xJxri <103562889+0xJxri@users.noreply.github.com>
2024-06-05 15:57:19 +0200

    added endpoints and jwt strategy

--- backend/todo-api/src/auth/auth.controller.ts
+++ backend/todo-api/src/auth/auth.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthPayloadDTO } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor( private authService: AuthService){}
 
-    @Post('login')
-    login(@Body() authPayload: AuthPayloadDTO){
-        
+    @Post('createUser')
+    @UsePipes(new ValidationPipe())
+    createUser(@Body() authPayload: AuthPayloadDTO){
+        console.log(authPayload )
+        return this.authService.createUser(authPayload)
     }
+
+      @Post('getJwt')
+      @UsePipes(new ValidationPipe())
+      getJwt(@Body() authPayload: AuthPayloadDTO){
+        return this.authService.getJwt(authPayload)
+      }
 }
--- backend/todo-api/src/auth/auth.service.ts
+++ backend/todo-api/src/auth/auth.service.ts
@@ -1,19 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { AuthPayloadDTO } from './dto/auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+    constructor(@InjectModel(User.name) private userModel: Model<User>, private jwtService: JwtService) { }
 
-    createUser(authPayloadDto: AuthPayloadDTO){
-        const newUser = new this.userModel()
+    async createUser(authPayloadDto: AuthPayloadDTO): Promise<User> {
+        const { username, email, password } = authPayloadDto;
+
+        const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+
+        const newUser = new this.userModel({ username, email, password: hashedPassword });
+
+        // Save the new user to the database
+        return newUser.save();
     }
 
+    async getJwt(authPayloadDto: AuthPayloadDTO): Promise<string> {
+        const { username, email, password } = authPayloadDto;
 
-    validateUser(authPayloadDto: AuthPayloadDTO) {
+        const user = await this.userModel.findOne({ $or: [{ username }, { email }] }).exec();
 
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        console.log("Password", password)
+        console.log(isPasswordValid, " - ", user.password)
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = { username: user.username, sub: user._id };
+        const token = this.jwtService.sign(payload);
+
+        return token;
     }
+
+    
 }
